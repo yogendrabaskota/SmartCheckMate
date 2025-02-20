575fbd761d5a25ac04833e3e7218d4c9858bd96d
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-02-20 21:29:33 +0545

    otp sent to email bug fix

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,4 +1,4 @@
-const { registerUser, getAllUser, loginUser, sendData } = require("../controller/authController")
+const { registerUser, getAllUser, loginUser, sendData, forgetPassword } = require("../controller/authController")
 const isAuthenticated = require("../moddleware/isAuthenticated")
 const catchAsync = require("../services/catchAsync")
 
@@ -16,5 +16,8 @@ router.route("/login")
     .post(loginUser)
 router.route("/data")
     .post(catchAsync(sendData))
+
+router.route("/forget")
+    .post(catchAsync(forgetPassword))
 
 module.exports = router
