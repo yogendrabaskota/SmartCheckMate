34e1d83a2d109436f9350f6f30d6db56c8169058
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-07-20 19:56:58 +0545

    all bug fix

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -186,9 +186,9 @@ const Footer = () => {
                   />
                 </svg>
                 <span className="text-gray-300">
-                  123 Business Ave, Suite 456
+                  123 Plaza, house-456
                   <br />
-                  New York, NY 10001
+                  Kathmandu, Nepal
                 </span>
               </li>
               <li className="flex items-center space-x-3">
@@ -205,7 +205,7 @@ const Footer = () => {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <span className="text-gray-300">+1 (555) 123-4567</span>
+                <span className="text-gray-300">+ (977) 9800000000</span>
               </li>
               <li className="flex items-center space-x-3">
                 <svg
